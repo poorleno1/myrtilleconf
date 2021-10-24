77ad1b47703dd0ff7969ac6d863c88797fea9f67
Jaroslaw Olechno <jaroslaw.olechno@circlekeurope.com>
2021-10-24 14:09:46 +0200

    final and minimal config

--- myrtille_sh.ps1
+++ myrtille_sh.ps1
@@ -20,34 +20,34 @@
             valueData = 1
         }
 
-        Registry RegistryKeyApplications {
-            Ensure    = "Present"  # You can also set Ensure to "Absent"
-            Key       = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Terminal Server\TSAppAllowList\Applications"
-            ValueName = ""
-        }
+        # Registry RegistryKeyApplications {
+        #     Ensure    = "Present"  # You can also set Ensure to "Absent"
+        #     Key       = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Terminal Server\TSAppAllowList\Applications"
+        #     ValueName = ""
+        # }
 
-        Registry RegistryKeyApplicationsCalc {
-            Ensure    = "Present"  # You can also set Ensure to "Absent"
-            Key       = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Terminal Server\TSAppAllowList\Applications\Calc"
-            ValueName = ""
-            DependsOn = "[Registry]RegistryKeyApplications"
-        }
+        # Registry RegistryKeyApplicationsCalc {
+        #     Ensure    = "Present"  # You can also set Ensure to "Absent"
+        #     Key       = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Terminal Server\TSAppAllowList\Applications\Calc"
+        #     ValueName = ""
+        #     DependsOn = "[Registry]RegistryKeyApplications"
+        # }
 
-        Registry RegistryKeyApplicationsCalcSetting1 {
-            Ensure    = "Present"  # You can also set Ensure to "Absent"
-            Key       = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Terminal Server\TSAppAllowList\Applications\Calc"
-            ValueName = "Name"
-            valueData = "calc.exe"
-            DependsOn = "[Registry]RegistryKeyApplicationsCalc"
-        }
+        # Registry RegistryKeyApplicationsCalcSetting1 {
+        #     Ensure    = "Present"  # You can also set Ensure to "Absent"
+        #     Key       = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Terminal Server\TSAppAllowList\Applications\Calc"
+        #     ValueName = "Name"
+        #     valueData = "calc.exe"
+        #     DependsOn = "[Registry]RegistryKeyApplicationsCalc"
+        # }
 
-        Registry RegistryKeyApplicationsCalcSetting2 {
-            Ensure    = "Present"  # You can also set Ensure to "Absent"
-            Key       = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Terminal Server\TSAppAllowList\Applications\Calc"
-            ValueName = "Path"
-            valueData = "c:\windows\system32"
-            DependsOn = "[Registry]RegistryKeyApplicationsCalc"
-        }
+        # Registry RegistryKeyApplicationsCalcSetting2 {
+        #     Ensure    = "Present"  # You can also set Ensure to "Absent"
+        #     Key       = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Terminal Server\TSAppAllowList\Applications\Calc"
+        #     ValueName = "Path"
+        #     valueData = "c:\windows\system32"
+        #     DependsOn = "[Registry]RegistryKeyApplicationsCalc"
+        # }
 
         WindowsFeature RDS-RD-Server {
             Name   = "RDS-RD-Server"
